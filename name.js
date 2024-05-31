460b379c854510c468edac956537f9e661d3f2af
QasemAbbas <86059801+QasemAbbas@users.noreply.github.com>
2024-05-31 17:34:16 +0300

    Add files via upload

---
+++ name.js
@@ -0,0 +1,152 @@
+//01. Create a variable called carName, assign the value Volvo to it.
+
+//:::::Answer::::::
+// let carName = "volvo" ; 
+// console.log(carName);
+
+
+//:::::::::::::::::::::::::::::::::::::
+
+// //02. On one single line, declare three variables with the following names and values:
+
+//:::::Answer::::::
+// var firstName = "John ", lastName = "Doe ", age = 35;
+// console.log(firstName + lastName + age);
+
+// let firstName = "John";  // variable name firstName
+// let lastName = "Doe"; //variable values is "Doe"
+// let age = 35; // variable name is age and value name 35 
+
+
+//:::::::::::::::::::::::::::::::::::::
+
+// //03. Use the correct assignment operator that will result in x being 50 (same as x = x * y).
+
+//:::::Answer::::::
+// x = 10;
+// y = 5;
+// x = x * y; 
+// console.log(x);
+
+
+//:::::::::::::::::::::::::::::::::::::
+
+
+// //04. Use comments to describe the correct data type of the following variables:
+
+//:::::Answer::::::
+// let length = 16; // Data type is number
+// console.log(typeof length);
+
+//:::::Answer::::::
+// let lastName = "Johnson"; // Data type is string
+// console.log(typeof lastName);
+
+//:::::Answer::::::
+// const x = {
+//   firstName: "John",  // variable name and value
+//   lastName: "Doe"  // variable name and value
+// };    // Data type is object
+// console.log(typeof x);
+
+
+//:::::::::::::::::::::::::::::::::::::
+
+// //05. Execute the function named myFunction.
+// function myFunction() {
+//   alert("Hello World!");
+// }
+// call the function and see the output 
+
+//:::::Answer::::::
+// function myFunction() {
+//   alert("Hello World!");
+// }
+// myFunction()
+
+//:::::::::::::::::::::::::::::::::::::
+
+
+// //06. Create an object called person with name = John, age = 50, Then, access the object to alert("John is 50").
+
+//:::::Answer::::::
+  //  var person = {
+  //    name: "John",
+  //    age: 50
+  //  }
+  //  alert(person.name + " is " + person.age);
+
+
+
+//:::::::::::::::::::::::::::::::::::::
+
+// //07. The <button> element should do something when someone clicks on it. Try to fix it!
+// <button>Click me.</button> 
+
+//:::::Answer::::::
+// function showMessage(){
+//   alert('welcome to Hablu Programmer');
+// }
+// showMessage()
+
+//:::::::::::::::::::::::::::::::::::::
+
+// //08. Array Related Question 
+// 1. Alert the number of items in an array, using the correct Array property: 
+
+//:::::Answer::::::
+// const cars = ["Volvo", "Jeep", "Mercedes"];
+// alert(cars.length + " Number of the Array Property");
+
+
+// 2. Change the first item of Brand to "Ford".
+
+//:::::Answer::::::
+// 	const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+//  Brand[0] = "Ford";
+//  console.log(Brand);
+
+
+
+//:::::::::::::::::::::::::::::::::::::
+
+// //09. Math Related Problems    
+// 1. Use the correct Math method to create a random number.
+
+//:::::Answer::::::
+// let sum = 7 % 2;
+// console.log(sum);
+
+// 2. Use the correct Math method to return the largest number of 10 and 20.
+
+//:::::Answer::::::
+// var largestNumber = Math.max(10,20);
+// console.log(largestNumber);
+
+// 3. Use the correct Math method to get the square root of 9.
+
+//:::::Answer::::::
+// var finalResult = Math.sqrt(9);
+// console.log(finalResult);
+
+//:::::::::::::::::::::::::::::::::::::
+
+//   //10. comparison operator related problems! 
+//   1. Choose the correct comparison operator to alert true, when x is greater than y. 
+
+//:::::Answer::::::
+// x = 10;
+// y = 5;
+// alert(x > y) // true
+
+// 2. Choose the correct conditional (ternary) operator to alert "Too young" if age is less than 18, otherwise alert "Old enough".
+
+//:::::Answer::::::
+// let age = 18;
+// let result;
+// if(age >= 17){
+//   result = "Too Young";
+// }else{
+//   result = "Old enough";
+// }
+// alert(result);
